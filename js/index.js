e0173edd6975a9e871f88c29ae494b8305743a40
dariososa <dariososa1342@gmail.com>
2020-10-05 21:59:53 -0300

    prueba

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+    
+
+    sr.reveal('.p2', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    sr.reveal('.btn', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    sr.reveal('.title', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    sr.reveal('li', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+   
